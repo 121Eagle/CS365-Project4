a2ac0b24bf6302b684ccb4d8a99cd98f0d66afc5
William Mironchuk <wmironchuk@umass.edu>
2022-03-09 17:24:21 -0500

    an attempt at fixing parse directory
    I think this thing is trying to parse content from a list rather then
    from the sector number

--- fsstat.py
+++ fsstat.py
@@ -338,7 +338,7 @@ class Fat:
                     ),
                 }
                 content, slack = self._get_content(
-                    answer["content_sectors"], answer["filesize"]
+                    answer["content_sectors"][0], answer["filesize"]
                 )
                 answer |= {"content": content, "slack": slack}
             directory_entries.append(answer)
