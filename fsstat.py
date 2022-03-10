ad8a2640f4851c972d42095498ec9aeea84377da
William Mironchuk <wmironchuk@umass.edu>
2022-03-09 20:29:38 -0500

    I am now just stripping out the large list of blank entries at the start of the directory
    This will later include me adding back in enough nulls to make the
    length of the directory divisable by 32

--- fsstat.py
+++ fsstat.py
@@ -320,7 +320,7 @@ class Fat:
         returns:
             list[dict]: list of dictionaries, one dict per entry
         """
-        directory = self._retrieve_data(cluster).strip()
+        directory = self._retrieve_data(cluster).rstrip(b"\x00")
         dir_sectors = self._get_sectors(cluster)
         directory_entries = []
         for entry_num, dir_entry in enumerate(
