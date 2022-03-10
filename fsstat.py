5f6064e5af95efc1f7b29f1add156616dfe70ef0
William Mironchuk <wmironchuk@umass.edu>
2022-03-09 23:27:28 -0500

    I am getting nothing here something

--- fsstat.py
+++ fsstat.py
@@ -278,10 +278,11 @@ class Fat:
             str (or None if unallocated cluster): slack content (up to 32 bytes)
 
         """
-        all_file_data = self._retrieve_data(cluster).decode(errors="ignore")
+        all_file_data = self._retrieve_data(cluster).decode("utf8", "ignore")
         if filesize == 0:
             return (all_file_data[: min(128, filesize)], None)
         slack = all_file_data[filesize : filesize + 32]
+        assert len(slack) <= 32, "The slack is longer than 32 bytes"
         return (all_file_data[: min(128, filesize)], slack)
 
     DONT_RECUR = frozenset({".", ".."})
