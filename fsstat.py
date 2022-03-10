b3e6ebc3069a80d0dbe1e04c4ca9579be02a4502
William Mironchuk <wmironchuk@umass.edu>
2022-03-09 22:35:29 -0500

    I have added the byte_formatting to the directory, but lets go

--- fsstat.py
+++ fsstat.py
@@ -328,7 +328,7 @@ class Fat:
         returns:
             list[dict]: list of dictionaries, one dict per entry
         """
-        directory = self._retrieve_data(cluster).rstrip(b"\x00")
+        directory = self.byte_formatting(self._retrieve_data(cluster))
         dir_sectors = self._get_sectors(cluster)
         directory_entries = []
         for entry_num, dir_entry in enumerate(
