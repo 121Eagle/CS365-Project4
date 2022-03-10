7cedcfc9174352fd342caf62154ee683ce73eea4
William Mironchuk <wmironchuk@umass.edu>
2022-03-09 22:45:58 -0500

    I have incorrect typing on this,
    specifically the get_content method

--- fsstat.py
+++ fsstat.py
@@ -278,7 +278,7 @@ class Fat:
             str (or None if unallocated cluster): slack content (up to 32 bytes)
 
         """
-        all_file_data = bytearray(self._retrieve_data(cluster))
+        all_file_data = self._retrieve_data(cluster)
         if filesize == 0:
             return (all_file_data[: min(128, filesize)], None)
         slack = all_file_data[filesize : filesize + 32]
