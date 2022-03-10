c770c4b2235bd1dba18f2b8e71b542d9f4bdf7ec
William Mironchuk <wmironchuk@umass.edu>
2022-03-09 21:27:03 -0500

    Stuff modifying how get content works

--- fsstat.py
+++ fsstat.py
@@ -280,11 +280,11 @@ class Fat:
         """
         all_file_data = bytearray(self._retrieve_data(cluster))
         if filesize == 0:
-            return (all_file_data[: min(128, filesize)].decode("ascii", "ignore"), None)
-        slack = all_file_data[filesize : filesize + 32]
+            return (all_file_data[: min(128, filesize)], None)
+        slack = all_file_data[filesize:filesize + 32]
         return (
-            all_file_data[0 : min(128, filesize)].decode("ascii", "ignore"),
-            slack.decode("ascii", "ignore"),
+            all_file_data[0 : min(128, filesize)],
+            slack
         )
 
     DONT_RECUR = frozenset({".", ".."})
